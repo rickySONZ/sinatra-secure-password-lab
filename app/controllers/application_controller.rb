993f5017882eba74a426f72c36fe370904eee6c1
Ryan Erricson <ryanerricson@gmail.com>
2021-03-17 17:50:51 -0400

    commit tree cf91ee1ae9cb104b3c18fd8977c98e3c30637333

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+		user = User.new(:username => params[:username], :password => params[:password])
+	  
+		if user.username == "" || user.password == nil
 
-  end
+		  redirect "/failure"
+		else
+		  redirect "/login"
+		end
+	  end 
 
   get '/account' do
     @user = User.find(session[:user_id])
@@ -32,8 +38,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
-  end
+		user = User.find_by(:username => params[:username])
+	  
+		if user && user.authenticate(params[:password])
+		  session[:user_id] = user.id
+		  redirect "/account"
+		else
+		  redirect "/failure"
+		end
+	  end 
 
   get "/failure" do
     erb :failure
